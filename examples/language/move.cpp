b6517cda06074bf9e197b767eb0d7360ae0b8e5f
Todd Kushner <kushnertodd@gmail.com>
2023-04-10 19:12:21 -0600

    move.cpp cleanup

--- examples/language/move.cpp
+++ examples/language/move.cpp
@@ -62,8 +62,7 @@ A&& rval_ref_to_rval_ref(A&& a) {
 int main() {
   std::cout << "  A a1(1);" << std::endl;
   A a1(1);
-  // illegal
-  // A& a2(2);
+  std::cout << "illegal: A& a2(2);" << std::endl;
   std::cout << "  A&& a3(3);" << std::endl;
   A&& a3(3);
   std::cout << "  A a4(a3);" << std::endl;
@@ -71,28 +70,24 @@ int main() {
   std::cout << "  A& a5(a4);" << std::endl;
   A& a5(a4);
   std::swap(a5, a5);
-  // illegal
-  // A&& a6(a5);
+  std::cout << "illegal: A&& a6(a5);" << std::endl;
 
   std::cout << "  A a6(A(6));" << std::endl;
   A a6(A(6));
-  // illegal
-  // A& a7(A(7));
+  std::cout << "illegal: A& a7(A(7));" << std::endl;
   std::cout << "  A&& a8(A(8));" << std::endl;
   A&& a8(A(8));
   std::swap(a8, a8);
 
   std::cout << "  A a6z(rval_to_rval(a8));" << std::endl;
   A a6z(rval_to_rval(a8));
-  // illegal
-  // A& a7z(rval_to_rval(a6z));
+  std::cout << "illegal: A& a7z(rval_to_rval(a6z));" << std::endl;
   std::cout << "  A&& a8z(rval_to_rval(a6z));" << std::endl;
   A&& a8z(rval_to_rval(a6z));
   std::swap(a8z, a8z);
 
   std::cout << "  const A a9(9);" << std::endl;
   const A a9(9);
-  // illegal
   std::cout << "  const A& a10(10);" << std::endl;
   const A& a10(10);
   A a10x = a10;
@@ -105,14 +100,11 @@ int main() {
   const A& a13(a12);
   A a13x = a13;
   std::swap(a13x, a13x);
-  // illegal
-  // A&& a14(a13);
+  std::cout << "illegal: A&& a14(a13);" << std::endl;
 
-  // illegal
   std::cout << "  const A a15(A(15));" << std::endl;
   const A a15(A(15));
-  // illegal
-  // A& a16(A(16));
+  std::cout << "illegal: A& a16(A(16));" << std::endl;
   std::cout << "  const A&& a17(A(17));" << std::endl;
   const A&& a17(A(17));
   A a17x = a17;
@@ -135,22 +127,19 @@ int main() {
   std::cout << "  A a23 = rval_to_rval(A(23));" << std::endl;
   A a23 = rval_to_rval(A(23));
   std::swap(a23, a23);
-  // illegal
-  // A& a24 = rval_to_rval(A(24));
+  std::cout << "illegal: A& a24 = rval_to_rval(A(24));" << std::endl;
   std::cout << "  A&& a25 = rval_to_rval(A(25));" << std::endl;
   A&& a25 = rval_to_rval(A(25));
   std::cout << "  A a26 = ref_to_rval(a25);" << std::endl;
   A a26 = ref_to_rval(a25);
-  // illegal
-  // A& a27 = ref_to_rval(a26);
+  std::cout << "illegal: A& a27 = ref_to_rval(a26);" << std::endl;
   std::cout << "  A&& a28 = ref_to_rval(a26);" << std::endl;
   A&& a28 = ref_to_rval(a26);
   std::swap(a28, a28);
   std::cout << "  A a29 = rval_ref_to_rval(A(29));" << std::endl;
   A a29 = rval_ref_to_rval(A(29));
   std::swap(a29, a29);
-  // illegal
-  // A& a30 = rval_ref_to_rval(A(30));
+  std::cout << "illegal: A& a30 = rval_ref_to_rval(A(30));" << std::endl;
   std::cout << "  A&& a31 = rval_ref_to_rval(A(31));" << std::endl;
   A&& a31 = rval_ref_to_rval(A(31));
   std::swap(a31, a31);
@@ -163,8 +152,7 @@ int main() {
   std::cout << "      ..." << std::endl;
   // A a32 = rval_to_rval_ref(A(32));
   // std::swap(a32, a32);
-  // illegal
-  // A& a33 = rval_to_rval_ref(A(33));
+  std::cout << "illegal: A& a33 = rval_to_rval_ref(A(33));" << std::endl;
   std::cout << "  A&& a34 = rval_to_rval_ref(A(34));" << std::endl;
   std::cout << "  infinite loop: " << std::endl;
   std::cout << "      -> constructor A(34) " << std::endl;
@@ -174,16 +162,14 @@ int main() {
   // std::swap(a34, a34);
   std::cout << "  A a35 = ref_to_rval_ref(a34);" << std::endl;
   A a35 = ref_to_rval_ref(a31);
-  // illegal
-  // A& a36 = ref_to_rval_ref(a35);
+  std::cout << "illegal: A& a36 = ref_to_rval_ref(a35);" << std::endl;
   std::cout << "  A&& a37 = ref_to_rval_ref(a35);" << std::endl;
   A&& a37 = ref_to_rval_ref(a35);
   std::swap(a37, a37);
   std::cout << "  A a38 = rval_ref_to_rval_ref(A(38));" << std::endl;
   A a38 = rval_ref_to_rval_ref(A(38));
   std::swap(a38, a38);
-  // illegal
-  // A& a39 = rval_ref_to_rval_ref(A(39));
+  std::cout << "illegal: A& a39 = rval_ref_to_rval_ref(A(39));" << std::endl;
   std::cout << "  A&& a40 = rval_ref_to_rval_ref(A(40));" << std::endl;
   A&& a40 = rval_ref_to_rval_ref(A(40));
   std::swap(a40, a40);
